7ded4ce60c5fc07d5f9d0cfc643f630780dc3ab6
abdelhadi-22 <abdoelbcir33@gmail.com>
2023-10-25 09:36:31 +0100

    first commit

---
+++ src/main/java/com/example/dynamyiqueproducttypes/DynamyiqueProductTypesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamyiqueproducttypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamyiqueProductTypesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamyiqueProductTypesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dynamyiqueproducttypes/DynamyiqueProductTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamyiqueproducttypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamyiqueProductTypesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
